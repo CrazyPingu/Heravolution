16b3536b6eb97afb118403381290c856a9f03ef0
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-07-05 00:23:27 +0200

    Fix icon bug with body

--- sites/HistoryOfOrders.php
+++ sites/HistoryOfOrders.php
@@ -10,51 +10,49 @@
     <head>
         <title>History of orders</title>
     </head>
-    <body>
-        <link rel="stylesheet" href="../css-folder/Table.css" />
-        <link rel="icon" href="../images/Heravolution_logo.png">
-        <h1>History of Orders</h1>
-        <?php
-            $query = "SELECT date, address, discountValue, totalPrice, weight, licensePlate FROM order_of_product WHERE fiscalCode = '" . $_SESSION["fiscalCode"] . "' ORDER BY IDOrderOfProduct";
-            $result = $conn->query($query);
-            if ($result->num_rows > 0) {
-                echo "<table><tr><th>Date</th><th>Address</th><th>Discount value</th><th>Total price</th><th>Weight</th><th>Delivered</th></tr>";
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    if (is_null($row["licensePlate"])) {
-                        $delivered = "no";
-                    } else {
-                        $delivered = "yes";
-                    }
-                    echo "<tr><td>" . $row["date"]. "</td><td>" . $row["address"]. "</td>
-                    <td>" . $row["discountValue"]. "</td><td>" . $row["totalPrice"]. "</td><td>".$row["weight"]."</td><td>" . $delivered. "</td></tr>";
+    <link rel="stylesheet" href="../css-folder/Table.css" />
+    <link rel="icon" href="../images/Heravolution_logo.png">
+    <h1>History of Orders</h1>
+    <?php
+        $query = "SELECT date, address, discountValue, totalPrice, weight, licensePlate FROM order_of_product WHERE fiscalCode = '" . $_SESSION["fiscalCode"] . "' ORDER BY IDOrderOfProduct";
+        $result = $conn->query($query);
+        if ($result->num_rows > 0) {
+            echo "<table><tr><th>Date</th><th>Address</th><th>Discount value</th><th>Total price</th><th>Weight</th><th>Delivered</th></tr>";
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                if (is_null($row["licensePlate"])) {
+                    $delivered = "no";
+                } else {
+                    $delivered = "yes";
                 }
-                echo "</table>";
-            } else {
-                echo "No orders";
+                echo "<tr><td>" . $row["date"]. "</td><td>" . $row["address"]. "</td>
+                <td>" . $row["discountValue"]. "</td><td>" . $row["totalPrice"]. "</td><td>".$row["weight"]."</td><td>" . $delivered. "</td></tr>";
             }
-            ?>
-        <h1>History of garbage orders</h1>
-        <?php
-            $query = "SELECT licensePlate, date, address, totalPrice, weight FROM pick_up_garbage WHERE fiscalCode = '" . $_SESSION["fiscalCode"] . "' ORDER BY IDOrderGarbage";
-            $result = $conn->query($query);
-            if ($result->num_rows > 0) {
-                echo "<table><tr><th>Date</th><th>Address</th><th>Total price</th><th>Weight</th><th>Delivered</th></tr>";
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    if (is_null($row["licensePlate"])) {
-                        $delivered = "no";
-                    } else {
-                        $delivered = "yes";
-                    }
-                    echo "<tr><td>" . $row["date"]. "</td><td>" . $row["address"]. "</td>
-                    <td>" . $row["totalPrice"]. "</td><td>".$row["weight"]."</td><td>" . $delivered. "</td></tr>";
+            echo "</table>";
+        } else {
+            echo "No orders";
+        }
+        ?>
+    <h1>History of garbage orders</h1>
+    <?php
+        $query = "SELECT licensePlate, date, address, totalPrice, weight FROM pick_up_garbage WHERE fiscalCode = '" . $_SESSION["fiscalCode"] . "' ORDER BY IDOrderGarbage";
+        $result = $conn->query($query);
+        if ($result->num_rows > 0) {
+            echo "<table><tr><th>Date</th><th>Address</th><th>Total price</th><th>Weight</th><th>Delivered</th></tr>";
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                if (is_null($row["licensePlate"])) {
+                    $delivered = "no";
+                } else {
+                    $delivered = "yes";
                 }
-                echo "</table>";
-            } else {
-                echo "No orders";
+                echo "<tr><td>" . $row["date"]. "</td><td>" . $row["address"]. "</td>
+                <td>" . $row["totalPrice"]. "</td><td>".$row["weight"]."</td><td>" . $delivered. "</td></tr>";
             }
-            ?>
-        <br><br><input type="button" value="Client page" onclick="window.location.href='ClientHome.php'">
-    </body>
+            echo "</table>";
+        } else {
+            echo "No orders";
+        }
+        ?>
+    <br><br><input type="button" value="Client page" onclick="window.location.href='ClientHome.php'">
     </html>
--- sites/InventoryWarehouse.php
+++ sites/InventoryWarehouse.php
@@ -10,24 +10,22 @@
     <head>
         <title>Inventory of the warehouse</title>
     </head>
-    <body>
-        <link rel="stylesheet" href="../css-folder/Table.css" />
-        <link rel="icon" href="../images/Heravolution_logo.png">
-        <h1>Inventory of the warehouse</h1>
-        <?php
-            $query = "SELECT address, productType, capacity, price, garbageType FROM warehouse, product 
-                WHERE IDOrder IS NULL AND product.IDWarehouse = warehouse.IDWarehouse ORDER BY warehouse.IDWarehouse";
-            $result = $conn->query($query);
-            if ($result->num_rows > 0) {
-                echo "<table><tr><th>Address</th><th>Product type</th><th>Capacity</th><th>Price</th><th>Garbage type</th></tr>";
-                while($row = $result->fetch_assoc()) {
-                    echo "<tr><td>" . $row["address"] . "</td><td>" . $row["productType"] . "</td><td>" . $row["capacity"] . "</td><td>" . $row["price"] . "</td><td>" . $row["garbageType"] . "</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "No products in stock";
+    <link rel="stylesheet" href="../css-folder/Table.css" />
+    <link rel="icon" href="../images/Heravolution_logo.png">
+    <h1>Inventory of the warehouse</h1>
+    <?php
+        $query = "SELECT address, productType, capacity, price, garbageType FROM warehouse, product 
+            WHERE IDOrder IS NULL AND product.IDWarehouse = warehouse.IDWarehouse ORDER BY warehouse.IDWarehouse";
+        $result = $conn->query($query);
+        if ($result->num_rows > 0) {
+            echo "<table><tr><th>Address</th><th>Product type</th><th>Capacity</th><th>Price</th><th>Garbage type</th></tr>";
+            while($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["address"] . "</td><td>" . $row["productType"] . "</td><td>" . $row["capacity"] . "</td><td>" . $row["price"] . "</td><td>" . $row["garbageType"] . "</td></tr>";
             }
-            ?>
-        <br><br><input type="button" value="Worker page" onclick="window.location.href='WarehouseWorkerHome.php'">
-    </body>
+            echo "</table>";
+        } else {
+            echo "No products in stock";
+        }
+        ?>
+    <br><br><input type="button" value="Worker page" onclick="window.location.href='WarehouseWorkerHome.php'">
 </html>
