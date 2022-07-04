4fa70c175e4475ed9f77c48c80e8524e932e8186
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-07-04 19:57:48 +0200

    Added the history of pick up garbage

--- sites/HistoryOfOrders.php
+++ sites/HistoryOfOrders.php
@@ -5,16 +5,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>History of Orders</title>
+        <title>History of orders</title>
     </head>
     <body>
     <input type="button" value="Client page" onclick="window.location.href='ClientHome.php'"><br><br>
     <h1>History of Orders</h1>
     <?php
-        $query = "SELECT * FROM order_of_product WHERE fiscalCode = '" . $_SESSION["fiscalCode"] . "' ORDER BY IDOrderOfProduct";
+        $query = "SELECT date, address, discountValue, totalPrice, weight, licensePlate FROM order_of_product WHERE fiscalCode = '" . $_SESSION["fiscalCode"] . "' ORDER BY IDOrderOfProduct";
         $result = $conn->query($query);
         if ($result->num_rows > 0) {
-            echo "<table><tr><th>Date</th><th>Address</th><th>Discount value</th><th>Total price</th><th>Delivered</th></tr>";
+            echo "<table><tr><th>Date</th><th>Address</th><th>Discount value</th><th>Total price</th><th>Weight</th><th>Delivered</th></tr>";
             // output data of each row
             while($row = $result->fetch_assoc()) {
                 if (is_null($row["licensePlate"])) {
@@ -23,7 +23,28 @@
                     $delivered = "yes";
                 }
                 echo "<tr><td>" . $row["date"]. "</td><td>" . $row["address"]. "</td>
-                    <td>" . $row["discountValue"]. "</td><td>" . $row["totalPrice"]. "</td><td>" . $delivered. "</td></tr>";
+                    <td>" . $row["discountValue"]. "</td><td>" . $row["totalPrice"]. "</td><td>".$row["weight"]."</td><td>" . $delivered. "</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No orders";
+        }
+    ?>
+    <h1>History of garbage orders</h1>
+    <?php
+        $query = "SELECT licensePlate, date, address, totalPrice, weight FROM pick_up_garbage WHERE fiscalCode = '" . $_SESSION["fiscalCode"] . "' ORDER BY IDOrderGarbage";
+        $result = $conn->query($query);
+        if ($result->num_rows > 0) {
+            echo "<table><tr><th>Date</th><th>Address</th><th>Total price</th><th>Weight</th><th>Delivered</th></tr>";
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                if (is_null($row["licensePlate"])) {
+                    $delivered = "no";
+                } else {
+                    $delivered = "yes";
+                }
+                echo "<tr><td>" . $row["date"]. "</td><td>" . $row["address"]. "</td>
+                    <td>" . $row["totalPrice"]. "</td><td>".$row["weight"]."</td><td>" . $delivered. "</td></tr>";
             }
             echo "</table>";
         } else {
